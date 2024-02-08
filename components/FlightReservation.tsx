f3f6f4d9b66ddcd9773a1882399b7853366bbae0
Rahul577503 <rahulmaurya109626@gmail.com>
2024-02-09 01:29:05 +0530

    Update FlightReservation.tsx

--- components/FlightReservation.tsx
+++ components/FlightReservation.tsx
@@ -4,7 +4,7 @@ import { GiCommercialAirplane } from "react-icons/gi";
 import { TbSend } from "react-icons/tb";
 
 const FlightReservation = () => {
-  const [videoError, setVideoError] = useState(false);
+  const [textColor, setTextColor] = useState("text-white");
 
   return (
     <main className="relative flex justify-center items-center min-h-screen">
@@ -14,39 +14,28 @@ const FlightReservation = () => {
         autoPlay
         muted
         loop
-        onError={() => setVideoError(true)} 
+        onError={() => setTextColor("text-gray-900")}
       />
-      {!videoError && (
-        <div className="flex flex-col items-center z-10 gap-2">
-          <div className="max-w-4xl text-center tracking-tight">
-            <h1 className="text-2xl md:text-3xl lg:text-4xl  text-white  leading-tight">
-              <span className="text-white">Fly Smart </span>
-              <GiCommercialAirplane className="inline-block text-amber-500 text-xl md:text-3xl lg:text-4xl align-middle" />
-              <span className="text-white">Unlock Hidden Flight Deals</span>
-              <br />
-              <span className="text-white">Exclusive Offers Await:</span>
-              <span className="text-white">
-                Dial Direct for Instant Access,
-              </span>
-              <span className="text-white">No Waiting!</span>
-            </h1>
-            <button className="mt-8 bg-red-800 text-lg hover:bg-rose-800 text-white font-semibold py-3 px-6 rounded inline-flex items-center">
-              Send Mail
-              <TbSend className="ml-2" />
-            </button>
-          </div>
-          <div className="md:mt-6 lg:mb-20">
-            <FlightForm />
-          </div>
+      <div className="flex flex-col items-center z-10 gap-2">
+        <div className="max-w-4xl text-center tracking-tight">
+          <h1 className={`text-2xl md:text-3xl lg:text-4xl leading-tight ${textColor}`}>
+            <span>Fly Smart </span>
+            <GiCommercialAirplane className="inline-block text-amber-500 text-xl md:text-3xl lg:text-4xl align-middle" />
+            <span>Unlock Hidden Flight Deals</span>
+            <br />
+            <span>Exclusive Offers Await:</span>
+            <span>Dial Direct for Instant Access,</span>
+            <span>No Waiting!</span>
+          </h1>
+          <button className="mt-8 bg-red-800 text-lg hover:bg-rose-800 text-white font-semibold py-3 px-6 rounded inline-flex items-center">
+            Send Mail
+            <TbSend className="ml-2" />
+          </button>
         </div>
-      )}
-      {videoError && (
-        <div className="absolute inset-0 flex justify-center items-center">
-          <span className="text-gray-900">
-            There was an error loading the video.
-          </span>
+        <div className="md:mt-6 lg:mb-20 ">
+          <FlightForm />
         </div>
-      )}
+      </div>
     </main>
   );
 };
