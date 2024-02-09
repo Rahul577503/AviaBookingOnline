2f0a51922a04c5ff90168da1387b4a83ca407f69
Rahul577503 <rahulmaurya109626@gmail.com>
2024-02-09 14:43:13 +0530

    Update FlightReservation.tsx

--- components/FlightReservation.tsx
+++ components/FlightReservation.tsx
@@ -2,14 +2,21 @@ import React, { useState } from "react";
 import FlightForm from "./FlightForm";
 import { GiCommercialAirplane } from "react-icons/gi";
 import { TbSend } from "react-icons/tb";
+import Link from "next/link";
 
 const FlightReservation = () => {
   const [videoLoaded, setVideoLoaded] = useState(false);
 
   return (
-    <main className={`relative flex justify-center items-center min-h-screen ${videoLoaded ? '' : 'bg-gray-800 text-white'}`}>
+    <main
+      className={`relative flex justify-center items-center min-h-screen ${
+        videoLoaded ? "" : "bg-gray-800 text-white"
+      }`}
+    >
       <video
-        className={`absolute inset-0 w-full h-full object-cover z-0 ${videoLoaded ? '' : 'hidden'}`}
+        className={`absolute inset-0 w-full h-full object-cover z-0 ${
+          videoLoaded ? "" : "hidden"
+        }`}
         autoPlay
         muted
         loop
@@ -22,7 +29,11 @@ const FlightReservation = () => {
 
       <div className="flex flex-col items-center z-10 gap-2">
         <div className="max-w-4xl text-center tracking-tight">
-          <h1 className={`text-2xl md:text-3xl text-white lg:text-4xl leading-tight ${videoLoaded ? '' : 'text-white'}`}>
+          <h1
+            className={`text-2xl md:text-3xl text-white lg:text-4xl leading-tight ${
+              videoLoaded ? "" : "text-white"
+            }`}
+          >
             <span>Fly Smart </span>
             <GiCommercialAirplane className="inline-block text-amber-500 text-xl md:text-3xl lg:text-4xl align-middle" />
             <span>Unlock Hidden Flight Deals</span>
@@ -32,7 +43,8 @@ const FlightReservation = () => {
             <span>No Waiting!</span>
           </h1>
           <button className="mt-8 bg-amber-500 text-lg hover:bg-amber-400 text-white font-semibold py-3 px-6 rounded inline-flex items-center">
-            Send Mail
+            <Link href="mailto:aviaBookingOnline.support@gmail.com">Send Mail</Link>
+
             <TbSend className="ml-2" />
           </button>
         </div>
