21d111833b069d04331a95d6e7fd301d31b952e4
Rahul577503 <rahulmaurya109626@gmail.com>
2024-02-09 13:52:39 +0530

    Update FlightReservation.tsx

--- components/FlightReservation.tsx
+++ components/FlightReservation.tsx
@@ -4,45 +4,42 @@ import { GiCommercialAirplane } from "react-icons/gi";
 import { TbSend } from "react-icons/tb";
 
 const FlightReservation = () => {
-  const [videoError, setVideoError] = useState(false);
+  const [videoLoaded, setVideoLoaded] = useState(false);
 
   return (
-    <main className={`relative flex justify-center items-center min-h-screen ${videoError ? 'bg-gray-800 text-white' : ''}`}>
-      {!videoError && (
-        <video
-          className="absolute inset-0 w-full h-full object-cover z-0"
-          autoPlay
-          muted
-          loop
-          playsInline
-          onError={() => setVideoError(true)}
-        >
-          <source src={require("../public/video.mp4")} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      )}
-      {!videoError && (
-        <div className="flex flex-col items-center z-10 gap-2">
-          <div className="max-w-4xl text-center tracking-tight">
-            <h1 className="text-2xl md:text-3xl lg:text-4xl leading-tight text-white">
-              <span>Fly Smart </span>
-              <GiCommercialAirplane className="inline-block text-amber-500 text-xl md:text-3xl lg:text-4xl align-middle" />
-              <span>Unlock Hidden Flight Deals</span>
-              <br />
-              <span>Exclusive Offers Await:</span>
-              <span>Dial Direct for Instant Access,</span>
-              <span>No Waiting!</span>
-            </h1>
-            <button className="mt-8 bg-red-800 text-lg hover:bg-rose-800 text-white font-semibold py-3 px-6 rounded inline-flex items-center">
-              Send Mail
-              <TbSend className="ml-2" />
-            </button>
-          </div>
-          <div className="md:mt-6 lg:mb-20 ">
-            <FlightForm />
-          </div>
+    <main className={`relative flex justify-center items-center min-h-screen ${videoLoaded ? '' : 'bg-gray-800 text-white'}`}>
+      <video
+        className={`absolute inset-0 w-full h-full object-cover z-0 ${videoLoaded ? '' : 'hidden'}`}
+        autoPlay
+        muted
+        loop
+        playsInline
+        onLoadedData={() => setVideoLoaded(true)}
+      >
+        <source src={require("../public/video.mp4")} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="flex flex-col items-center z-10 gap-2">
+        <div className="max-w-4xl text-center tracking-tight">
+          <h1 className={`text-2xl md:text-3xl text-white lg:text-4xl leading-tight ${videoLoaded ? '' : 'text-white'}`}>
+            <span>Fly Smart </span>
+            <GiCommercialAirplane className="inline-block text-amber-500 text-xl md:text-3xl lg:text-4xl align-middle" />
+            <span>Unlock Hidden Flight Deals</span>
+            <br />
+            <span>Exclusive Offers Await:</span>
+            <span>Dial Direct for Instant Access,</span>
+            <span>No Waiting!</span>
+          </h1>
+          <button className="mt-8 bg-red-800 text-lg hover:bg-rose-800 text-white font-semibold py-3 px-6 rounded inline-flex items-center">
+            Send Mail
+            <TbSend className="ml-2" />
+          </button>
         </div>
-      )}
+        <div className="md:mt-6 lg:mb-20 ">
+          <FlightForm />
+        </div>
+      </div>
     </main>
   );
 };
